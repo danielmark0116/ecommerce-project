7a4da48984771ad548fdf10cd9b0184e36271d6e
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-10-22 10:35:19 +0200

    Add config file for triggering devtools on client side

---
+++ client/src/App/config.ts
@@ -0,0 +1,1 @@
+export const devToolsMode = true;
--- client/src/App/store/index.ts
+++ client/src/App/store/index.ts
@@ -2,14 +2,28 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk, { ThunkMiddleware } from 'redux-thunk';
 import { rootReducer, AppState } from '../reducers/index';
 import { ActionTypes } from '../actions/actionTypes';
+import { devToolsMode } from '../config';
 
 const middleware =
   process.env.REACT_APP_MODE === 'development'
-    ? compose(
-        applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>),
-        (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-          (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-      )
+    ? devToolsMode
+      ? compose(
+          applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>),
+          (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+            (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+        )
+      : compose(
+          applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>)
+        )
     : compose(applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>));
+
+// const middleware =
+//   process.env.REACT_APP_MODE === 'development'
+//     ? compose(
+//         applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>),
+//         (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+//           (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+//       )
+//     : compose(applyMiddleware(thunk as ThunkMiddleware<AppState, ActionTypes>));
 
 export const store = createStore(rootReducer, middleware);
