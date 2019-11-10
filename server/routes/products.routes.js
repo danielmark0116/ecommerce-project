5ecb7c84255c35a4842b9acb218adbea85b88629
Daniel Grychtol <grychtol.daniel@gmail.com>
2019-11-10 11:23:59 +0100

    Finish setting up readme

--- server/routes/products.routes.js
+++ server/routes/products.routes.js
@@ -1,29 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const productController = require('../controller/product.controller');
+
+const isAdmin = require('../middleware/isAdmin');
 
 router.get('/', productController.getPublished);
 
-router.get('/all', productController.getAll);
+router.get(
+  '/all',
+  passport.authenticate('jwt', { session: false }),
+  isAdmin.checkIfUserIsAdmin,
+  productController.getAll
+); // for admin panel, not yet implemented
 
 router.get('/cart', productController.getCart);
 
-// router.get('/range/:start/:limit', productController.getRangeProducts);
-
-// router.get('/all/range/:start/:limit', productController.getRangeAllProducts);
-
 router.get('/:skip/:limit/filter?', productController.productsFilters);
 
-router.get('/all/:skip/:limit/filter?', productController.productsAllFilters);
+router.get(
+  '/all/:skip/:limit/filter?',
+  passport.authenticate('jwt', { session: false }),
+  isAdmin.checkIfUserIsAdmin,
+  productController.productsAllFilters
+); // for admin panel, not yet implemented
 
 router.get('/:id', productController.getById);
 
-router.post('/', productController.postProduct);
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  isAdmin.checkIfUserIsAdmin,
+  productController.postProduct
+);
 
-router.put('/:id', productController.editProduct);
+router.put(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  isAdmin.checkIfUserIsAdmin,
+  productController.editProduct
+);
 
-router.patch('/publish/:id', productController.publishProduct);
+router.patch(
+  '/publish/:id',
+  passport.authenticate('jwt', { session: false }),
+  isAdmin.checkIfUserIsAdmin,
+  productController.publishProduct
+);
 
-router.delete('/:id', productController.deleteProduct);
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  isAdmin.checkIfUserIsAdmin,
+  productController.deleteProduct
+);
 
 module.exports = router;
